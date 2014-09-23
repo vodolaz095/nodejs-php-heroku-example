1ccd8b8ad0b145ad9a5fe25b3dfa4943825cc434
Ostroumov Anatolij <ostroumov095@gmail.com>
2014-09-23 17:34:54 +0400

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http'),
+  exec = require('child_process').exec;
+
+http.createServer(function (req, res) {
+
+
+  child = exec('php script.php',
+    function (error, stdout, stderr) {
+      if(error){
+        throw error;
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(stdout);
+      }
+    });
+
+}).listen(process.env.PORT || 3000);
